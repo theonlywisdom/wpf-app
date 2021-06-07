4750365a3f3bb0f8c1c299bbb035dba4ce1cf94a
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-07 14:19:58 +0100

    Constraints added

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,13 +1,22 @@
-﻿namespace FriendOrganizer.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendOrganizer.Model
 {
   public class Friend
   {
+    [Required]
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(50)]
     public string FirstName { get; set; }
 
+    [Required]
+    [MaxLength(50)]
     public string LastName { get; set; }
 
+    [Required]
+    [MaxLength(50)]
     public string Email { get; set; }
   }
 }
