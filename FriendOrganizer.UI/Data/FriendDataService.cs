ae4584d2161ff94670f719ebc65e9d702bca0569
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-07 16:51:57 +0100

    FriendDataService completed

--- FriendOrganizer.UI/Data/FriendDataService.cs
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -1,5 +1,6 @@
 ﻿using FriendOrganizer.DataAccess;
 using FriendOrganizer.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,9 +8,16 @@ namespace FriendOrganizer.UI.Data
 {
   public class FriendDataService : IFriendDataService
   {
+        private Func<FriendOrganizerDbContext> _contextCreator;
+
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+    {
+            _contextCreator = contextCreator;
+    }
+
     public IEnumerable<Friend> GetAll()
     {
-            using (var ctx = new FriendOrganizerDbContext())
+            using (var ctx = _contextCreator())
             {
                 return ctx.Friends.AsNoTracking().ToList();
             }
