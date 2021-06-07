bd8eeb7127ef9ff47f312a581c268e0188d4d6dc
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-07 13:56:29 +0100

    Add project files.

---
+++ FriendOrganizer.UI/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FriendOrganizer.UI.ViewModel;
+using System.Windows;
+
+namespace FriendOrganizer.UI
+{
+  public partial class MainWindow : Window
+  {
+    private MainViewModel _viewModel;
+
+    public MainWindow(MainViewModel viewModel)
+    {
+      InitializeComponent();
+      _viewModel = viewModel;
+      DataContext = _viewModel;
+      Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      _viewModel.Load();
+    }
+  }
+}
