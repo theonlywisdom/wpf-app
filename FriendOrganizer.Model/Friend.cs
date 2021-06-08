26f3f4c5623a91c1370c4956951959f8a0ff700b
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-08 14:34:54 +0100

    Friend prop attributes changed to StringLength

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -8,13 +8,13 @@ namespace FriendOrganizer.Model
     public int Id { get; set; }
 
     [Required]
-    [MaxLength(50)]
+    [StringLength(50)]
     public string FirstName { get; set; }
 
-    [MaxLength(50)]
+    [StringLength(50)]
     public string LastName { get; set; }
 
-    [MaxLength(50)]
+    [StringLength(50)]
     public string Email { get; set; }
   }
 }
