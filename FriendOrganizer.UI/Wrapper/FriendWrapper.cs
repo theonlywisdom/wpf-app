9ce50d549c68a07b7fe689abbe4224ef7ae57a4d
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-08 15:37:21 +0100

    FriendWrapper added

---
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -0,0 +1,61 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class FriendWrapper : ViewModelBase
+    {
+        public FriendWrapper(Friend model)
+        {
+            Model = model;
+        }
+
+        public Friend Model { get; }
+
+        public int Id { get { return Model.Id; } }
+
+        public string FirstName 
+        {
+            get 
+            {
+                return Model.FirstName;
+            }
+            set
+            {
+                Model.FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName 
+        {
+            get 
+            {
+                return Model.LastName;
+            }
+            set
+            {
+                Model.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email 
+        {
+            get 
+            {
+                return Model.Email;
+            }
+            set
+            {
+                Model.Email = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
