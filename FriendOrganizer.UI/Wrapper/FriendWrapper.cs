79351d81196f91e84c6c8830cb2ee0e4fcc5c183
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-09 10:02:07 +0100

    ModelWrapper class added

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,21 +1,15 @@
 ﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
-    {
-        public FriendWrapper(Friend model)
-        {
-            Model = model;
-        }
 
-        public Friend Model { get; }
+    public class FriendWrapper : ModelWrapper<Friend>
+    {
+        public FriendWrapper(Friend model) : base(model)
+        {
+
+        }
 
         public int Id { get { return Model.Id; } }
 
@@ -23,15 +17,16 @@ namespace FriendOrganizer.UI.Wrapper
         {
             get 
             {
-                return Model.FirstName;
+                return GetValue<string>();
             }
             set
             {
-                Model.FirstName = value;
-                OnPropertyChanged();
+                SetValue(value);
                 ValidateProperty(nameof(FirstName));
             }
         }
+
+        
 
         private void ValidateProperty(string propertyName)
         {
@@ -51,12 +46,11 @@ namespace FriendOrganizer.UI.Wrapper
         {
             get 
             {
-                return Model.LastName;
+                return GetValue<string>();
             }
             set
             {
-                Model.LastName = value;
-                OnPropertyChanged();
+                SetValue(value);
             }
         }
 
@@ -64,53 +58,11 @@ namespace FriendOrganizer.UI.Wrapper
         {
             get 
             {
-                return Model.Email;
+                return GetValue<string>();
             }
             set
             {
-                Model.Email = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Dictionary<string, List<string>> _errorsByPropertyName  
-            = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-                ? _errorsByPropertyName[propertyName]
-                : null;
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName)); 
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
+                SetValue(value);
             }
         }
     }
---
+++ FriendOrganizer.UI/Wrapper/ModelWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class ModelWrapper<T> : NotifyErrorInfoBase
+    {
+        public ModelWrapper(T model)
+        {
+            Model = model;
+        }
+
+        public T Model { get; }
+
+        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName] string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(Model, value);
+            OnPropertyChanged(propertyName);
+        }
+
+        protected virtual TValue GetValue<TValue>([CallerMemberName] string propertyName = null)
+        {
+            return (TValue)typeof(T).GetProperty(propertyName).GetValue(Model);
+        }
+    }
+}
