f7068aa6265772d7a879c0255ae088cbe5a22a3d
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-11 12:40:54 +0100

    Extend the LookupDataService

--- FriendOrganizer.UI/Data/Lookups/LookupDataService.cs
+++ FriendOrganizer.UI/Data/Lookups/LookupDataService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.Data.Lookups
 {
-    public class LookupDataService : IFriendLookupDataService
+    public class LookupDataService : IFriendLookupDataService, IProgrammingLanguageLookupDataService
     {
         private Func<FriendOrganizerDbContext> _contextCreator;
 
@@ -22,6 +22,21 @@ namespace FriendOrganizer.UI.Data.Lookups
                     {
                         Id = f.Id,
                         DisplayMember = f.FirstName + " " + f.LastName
+                    })
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<IEnumerable<LookupItem>> GetProgrammingLanguageLookupAsync()
+        {
+            using (var ctx = _contextCreator())
+            {
+                return await ctx.ProgrammingLanguages.AsNoTracking()
+                    .Select(f =>
+                    new LookupItem
+                    {
+                        Id = f.Id,
+                        DisplayMember = f.Name
                     })
                     .ToListAsync();
             }
