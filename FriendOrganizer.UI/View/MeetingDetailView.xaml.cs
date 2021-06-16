bdb0ca30eb53ada1fb3a17a73294da6f038d951c
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-16 15:55:48 +0100

    Create the MeetingDetailView

---
+++ FriendOrganizer.UI/View/MeetingDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FriendOrganizer.UI.View
+{
+    /// <summary>
+    /// Interaction logic for MeetingDetailView.xaml
+    /// </summary>
+    public partial class MeetingDetailView : UserControl
+    {
+        public MeetingDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- FriendOrganizer.UI/ViewModel/MeetingDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MeetingDetailViewModel.cs
@@ -5,9 +5,6 @@ using FriendOrganizer.UI.Wrapper;
 using Prism.Commands;
 using Prism.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.ViewModel
