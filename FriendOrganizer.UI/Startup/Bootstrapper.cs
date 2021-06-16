d215c20a9b5a9a39fa5fdcae5bd113837e2b449f
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-16 13:00:33 +0100

    Create the MeetingRepository

--- FriendOrganizer.UI/Startup/Bootstrapper.cs
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -28,6 +28,7 @@ namespace FriendOrganizer.UI.Startup
 
       builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
       builder.RegisterType<FriendRepository>().As<IFriendRepository>();
+      builder.RegisterType<MeetingRepository>().As<IMeetingRepository>();
 
       return builder.Build();
     }
