b83f630b6ea1212d7315a36462d0ab632d19d547
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-18 11:08:22 +0100

    Publish the AfterDetailClosedEvent

--- FriendOrganizer.UI/ViewModel/DetailViewModelBase.cs
+++ FriendOrganizer.UI/ViewModel/DetailViewModelBase.cs
@@ -1,10 +1,6 @@
 ﻿using FriendOrganizer.UI.Event;
 using Prism.Commands;
 using Prism.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -17,19 +13,24 @@ namespace FriendOrganizer.UI.ViewModel
         private int _id;
         private string _title;
 
-        public ICommand DeleteCommand { get; private set; }
-
-        public int Id
-        {
-            get { return _id; }
-            protected set { _id = value; }
-        }
-
         public DetailViewModelBase(IEventAggregator eventAggregator)
         {
             EventAggregator = eventAggregator;
             SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
             DeleteCommand = new DelegateCommand(OnDeleteExecute);
+            CloseDetailViewCommand = new DelegateCommand(OnCloseDetailViewExecute);
+        }
+
+        public ICommand DeleteCommand { get; private set; }
+
+        public ICommand SaveCommand { get; private set; }
+
+        public ICommand CloseDetailViewCommand { get; }
+
+        public int Id
+        {
+            get { return _id; }
+            protected set { _id = value; }
         }
 
         public string Title 
@@ -85,6 +86,14 @@ namespace FriendOrganizer.UI.ViewModel
                 });
         }
 
-        public ICommand SaveCommand { get; private set; }
+        protected virtual void OnCloseDetailViewExecute()
+        {
+            EventAggregator.GetEvent<AfterDetailClosedEvent>()
+                .Publish(new AfterDetailClosedEventArgs
+                {
+                    Id = this.Id,
+                    ViewModelName = this.GetType().Name
+                });
+        }
     }
 }
