38d53c66bc6739c40e1969aed963ead6a1eee456
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-06-21 11:05:48 +0100

    Add a Row Version to the Friend Entity

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -6,18 +6,6 @@ namespace FriendOrganizer.Model
 {
     public class Friend
     {
-        [EmailAddress]
-        [StringLength(50)]
-        public string Email { get; set; }
-
-        public ProgrammingLanguage FavoriteLanguage { get; set; }
-
-        public int? FavoriteLanguageId { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string FirstName { get; set; }
-
         public Friend()
         {
             PhoneNumbers = new Collection<FriendPhoneNumber>();
@@ -27,11 +15,26 @@ namespace FriendOrganizer.Model
         [Required]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
         [StringLength(50)]
         public string LastName { get; set; }
 
-        public ICollection<Meeting> Meetings { get; set; }
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        public int? FavoriteLanguageId { get; set; }
+
+        public ProgrammingLanguage FavoriteLanguage { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
 
         public ICollection<FriendPhoneNumber> PhoneNumbers { get; set; }
+
+        public ICollection<Meeting> Meetings { get; set; }
     }
 }
